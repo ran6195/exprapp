128068c6042d7d00a9f7041e880999295bddf65c
Francesco Corsanello <francesco@MBPdiFrancesco.homenet.telecomitalia.it>
2019-11-13 21:47:25 +0100

    Fine giornata

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' , menuItem : 'home'});
+});
+
+/** pagina reperibilità */
+
+router.get( '/reperibili' , (req, res, next) => {
+  res.render( 'reperibili' , {
+    title : 'Reperibili' , menuItem : 'reperibili'
+  });
 });
 
 module.exports = router;
