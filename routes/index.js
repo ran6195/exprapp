2d3c742f178781062c7daf99b1bbd64606168a1f
Francesco Corsanello <francesco@MBPdiFrancesco.homenet.telecomitalia.it>
2019-11-18 08:24:19 +0100

    Query

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const sqlite3 = require( 'sqlite3' ).verbose(); 
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,8 +22,38 @@ router.get( '/reperibili' , (req, res, next) => {
 /** pagina zfs */
 
 router.get( '/zfsappliance' , (req, res, next) => {
+
+
+  let db = new sqlite3.Database( './storagetools.db' , sqlite3.OPEN_READONLY ,  err => {
+    if( err ) {
+      console.log( 'Errore: ' + err )
+    }
+  });
+
+  let dati = [ 'pippo' ];
+
+  db.serialize(() => {
+    db.each( 'SELECT * FROM zfsappliance' , ( err , row ) => {
+      if( err ) {
+        console.log( err )
+      }
+      dati.push( row )
+    }) 
+    
+  })
+
+  db.close( err => {
+    if( err ) {
+      console.log( err )
+    }
+  })
+
+
+
   res.render( 'zfsappliance' , {
-    title : 'ZFS Appliance' , menuItem : 'zfsappliance'
+    title : 'ZFS Appliance' , 
+    menuItem : 'zfsappliance' ,
+    dati
   });
 });
 
