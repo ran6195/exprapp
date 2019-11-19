2e85aef6e0f8300124279ac906ba0c9875a28a72
Francesco Corsanello <francesco@MBPdiFrancesco.homenet.telecomitalia.it>
2019-11-19 07:11:26 +0100

    Aggiunto router per API

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+var express = require( 'express' );
+var router  = express.Route();
+const sqlite3 = require( 'sqlite3' ).verbose();
+
+
+
+
+/** Get All ZFS */
+
+router.get( '/zfs' , ( req , res , next )=> {
+    
+})
